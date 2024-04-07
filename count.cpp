2a7d14c25e824ce94ee631cc104bd7b2c4c227f3
@lockupss <@lockupss>
2024-04-07 21:18:58 +0300

    funrise

---
+++ count.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>	
+using namespace std;
+int count(int can[100]);
+char can[100];
+int main(){
+	int man;
+	cout<<"ENTER THE STRING";
+	cin.getline(can,100);
+	count(can[100]);
+		cout<<"The length of the string is : "<<count;
+return 0;	
+}
+
+int count(int can[100]){
+return strlen(can[100);
+}
+
